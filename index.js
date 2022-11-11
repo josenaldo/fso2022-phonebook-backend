7e8bed9d8e7ff3dfe6a0b1a9dac3786c51dc6458
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2022-11-11 15:55:35 -0300

    :sparkles: feat: 3.2: Phonebook backend step2
    The info page page displays the number of phonebook entries and the time the request was received.

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.get('/api/persons', (request, response) => {
     response.send(notes)
 })
 
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${notes.length} people</p>
+    <p>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
